45ea5367244910f52e3ec5cec0859e4f2b8bcc13
Joey Carlino <robospunk@gmail.com>
2018-10-01 18:27:52 -0400

    added collapsable nav and sidebar header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 let navs = document.querySelectorAll('nav, .nav');
 
-for (let i = 0; i < navs.length; i++) {
-    
-}
+// for (let i = 0; i < navs.length; i++) {
+//     if (navs[i].querySelector(".collapse") != null) {
+//         navs[i].querySelector(".collapse").style.display = "none";
+//     }
+// }
 
 document.addEventListener('click', listenForClicks);
 
 function listenForClicks(e) {
     if (e.target.classList.contains('collapseToggle')) {
-        console.log('toggle');
+        e.target.parentNode.querySelector('.collapse').classList.toggle('expand');
     }
 }
